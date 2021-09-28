343fe8bd541d778b74a82955a1f7d841726db9b9
yyhser <49522014+yyhser@users.noreply.github.com>
2021-09-28 19:50:00 +0800

    Update axios.ts

--- src/untils/axios.ts
+++ src/untils/axios.ts
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import { ElMessage } from 'element-plus'
+// import { ElMessage } from 'element-plus'
 const baseURL = 'https://api.github.com'
 const axios = Axios.create({
   baseURL,
@@ -32,10 +32,10 @@ axios.interceptors.response.use(
     if (error.response && error.response.data) {
       const code = error.response.status
       const msg = error.response.data.message
-      ElMessage.error(`Code: ${code}, Message: ${msg}`)
+//       ElMessage.error(`Code: ${code}, Message: ${msg}`)
       console.error(`[Axios Error]`, error.response)
     } else {
-      ElMessage.error(`${error}`)
+//       ElMessage.error(`${error}`)
     }
     return Promise.reject(error)
   }
