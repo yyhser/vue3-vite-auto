fe2f9bb06c75cc7c22c2ff542dbafbe85fa50d81
yyhser <49522014+yyhser@users.noreply.github.com>
2021-09-28 19:52:40 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import store from './store/index'
 import router from './router/index'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
