f55a9e30ff1fba848858f103cbb41a0f0f5e03e9
SC-201805191749\Administrator <yyhser@126.com>
2021-09-28 17:39:12 +0800

    first

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text">13</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from '../untils/axios'
+export default defineComponent({
+  setup() {
+    axios
+      .get('/users/XPoet')
+      .then((res) => {
+        console.log('res: ', res)
+      })
+      .catch((err) => {
+        console.log('err: ', err)
+      })
+  }
+})
+</script>
+<style lang="less">
+.text {
+  color: #f00;
+}
+</style>
